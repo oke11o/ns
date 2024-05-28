8a400426809c12b919b50281bf0c0cc0dcac9bc7
DmitruNS <voroshilovdo@gmail.com>
2024-05-28 10:38:33 +0200

    1

---
+++ home/testBD.js
@@ -0,0 +1,38 @@
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAk68Jk6DvWUZAlGfu-tOKmC45fo1sX18w",
+    authDomain: "voroshilovdo-39efc.firebaseapp.com",
+    projectId: "voroshilovdo-39efc",
+    storageBucket: "voroshilovdo-39efc.appspot.com",
+    messagingSenderId: "859981515674",
+    appId: "1:859981515674:web:c4a6120186614d24a78823"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+function logMessage(message) {
+    var logDiv = document.getElementById('log');
+    logDiv.innerHTML += `<p>${message}</p>`;
+}
+
+// Test Firestore connection
+db.collection("test").add({
+    testField: "testValue"
+})
+.then((docRef) => {
+    logMessage("Document written with ID: " + docRef.id);
+})
+.catch((error) => {
+    logMessage("Error adding document: " + error);
+});
+
+// Read from Firestore
+db.collection("test").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        logMessage(`Document data: ${JSON.stringify(doc.data())}`);
+    });
+}).catch((error) => {
+    logMessage("Error getting documents: " + error);
+});
